4a5b96678fd21ae4aa94e716d642b4875344fc21
@hexlet/cli <support@hexlet.io>
2023-12-20 22:05:05 +0300

    submit rails-basics-ru/rack

--- rails-basics-ru/rack/lib/app/router.rb
+++ rails-basics-ru/rack/lib/app/router.rb
@@ -5,28 +5,29 @@ require 'rack'
 class Router
   def call(env)
     # BEGIN
-        path = env['PATH_INFO']
-        case path
-        when '/'
-          handle_root
-        when '/about'
-          handle_about
-        else
-          handle_not_found
-        end
+    path = env['PATH_INFO']
+    case path
+    when '/'
+      handle_root
+    when '/about'
+      handle_about
+    else
+      handle_not_found
+    end
   end
-      private
 
-      def handle_root
-        [200, {'Content-Type' => 'text/plain'}, ['Hello from the root!']]
-      end
+  private
 
-      def handle_about
-        [200, {'Content-Type' => 'text/plain'}, ['About us page']]
-      end
+  def handle_root
+    [200, {'Content-Type' => 'text/plain'}, ['Hello, World!']]
+  end
 
-      def handle_not_found
-        [404, {'Content-Type' => 'text/plain'}, ['Not Found']]
-      end
+  def handle_about
+    [200, {'Content-Type' => 'text/plain'}, ['About page']]
+  end
+
+  def handle_not_found
+    [404, {'Content-Type' => 'text/plain'}, ['404 Not Found']]
+  end
     # END
 end

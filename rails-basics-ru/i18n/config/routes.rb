2315e673dea249ecb8f12957a5d3a33db2f0e864
@hexlet/cli <support@hexlet.io>
2024-08-08 20:09:04 +0300

    submit rails-basics-ru/i18n

--- rails-basics-ru/i18n/config/routes.rb
+++ rails-basics-ru/i18n/config/routes.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
   # BEGIN
-  Rails.application.routes.draw do
-    scope "(:locale)", locale: /en|ru/ do
-      root 'home#index'
-      resources :posts
+  locales = I18n.available_locales.join('|')
+  scope '/(:locale)', locale: /#{locales}/ do
+    root to: 'home#index'
+
+    resources :posts do
+      scope module: :posts do
+        resources :comments, path: 'post_comments'
+      end
     end
   end
-  
-
   # END
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

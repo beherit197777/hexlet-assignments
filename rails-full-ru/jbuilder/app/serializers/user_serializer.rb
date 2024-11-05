e0d82ded05c4a42c3a2aa2233cb736f00ecb0588
@hexlet/cli <support@hexlet.io>
2024-11-05 15:31:29 +0300

    submit rails-full-ru/jbuilder

---
+++ rails-full-ru/jbuilder/app/serializers/user_serializer.rb
@@ -0,0 +1,8 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :email, :address
+  has_many :posts
+
+  attribute :full_name do
+    "#{object.first_name} #{object.last_name}"
+
+end

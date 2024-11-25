ca162dbee32f71edcc2b9c1dafd253493c549107
@hexlet/cli <support@hexlet.io>
2024-11-25 16:09:05 +0300

    submit rails-full-ru/jobs

---
+++ rails-full-ru/jobs/app/models/repository.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class Repository < ApplicationRecord
+  validates :link, presence: true, uniqueness: true
+
+  # BEGIN
+  include AASM
+
+  aasm do
+    state :created, initial: true
+    state :fetching, :fetched, :failed
+
+    event :fetch do
+      transitions from: %i[created fetched failed], to: :fetching
+    end
+
+    event :rollback do
+      transitions from: :fetching, to: :created
+    end
+
+    event :mark_as_fetched do
+      transitions from: :fetching, to: :fetched
+    end
+
+    event :mark_as_failed do
+      transitions to: :failed
+    end
+  end
+  # END
+end
